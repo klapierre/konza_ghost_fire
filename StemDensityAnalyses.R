0d9e66307ff34c14d361aeba96b35ba46c4b1135
Meghan Avolio <mavolio2@jh.edu>
2022-05-22 14:17:38 -0400

    Making figures comapring N+L treatments in each burn trt.

--- StemDensityAnalyses.R
+++ StemDensityAnalyses.R
@@ -24,9 +24,42 @@ total=sd2 %>%
   group_by(Year, Watershed, Block, Plot, Litter, Nutrient, treatment, Burn) %>% 
   summarise(total=sum(stems))
 
-fit <- lmer(total ~  Burn*Litter*Nutrient*Year,  random = ~1|Watershed/Block, data = total)
+fit <- lmer(total ~  Burn*Litter*Nutrient*Year +(1|Watershed/Block), data = total)
 anova(fit)
 
-#Just trt yrs
-fit <- lmer(total ~  Burn.Trt2*Litter*Nutrient+(1|Block), data = subset(ALL, Year!=2014))
-Anova(fit)
+###analysis annual burn only
+fit1 <- lmer(total ~  Litter*Nutrient*Year +(1|Watershed/Block), data = subset(total, Burn==1))
+anova(fit1)
+
+###analysis of unburned only
+fit20 <- lmer(total ~  Litter*Nutrient*Year +(1|Watershed/Block), data = total)
+anova(fit20)
+
+
+means<-total %>% 
+  group_by(Year, Burn, Litter, Nutrient) %>%
+  summarize(Mtot=mean(total, na.rm=T), sddev=sd(total), n=length(total)) %>% 
+  mutate(se=sddev/sqrt(n)) %>% 
+  mutate(treatment=ifelse(Burn==1&Nutrient=="C"&Litter=="A", "Annual Burn",
+                          ifelse (Burn==1&Nutrient=="U"&Litter=="P", "Annual+N+L",
+                                  ifelse(Burn==1&Nutrient=="U"&Litter=="A", "Annual+Nitrogen",
+                                         ifelse(Burn==1&Nutrient=="C"&Litter=="P", "Annual+Litter",ifelse(Burn==20&Nutrient=="C"&Litter=="P", "Unburned", ifelse(Burn==20&Nutrient=="S"&Litter=="A", "Unburned-N-L", ifelse(Burn==20&Nutrient=="S"&Litter=="P", "Unburned-Nitrogen", ifelse(Burn==20&Nutrient=="C"&Litter=="A", "Unburned-Litter", "drop")))))))))
+
+#annaul
+ggplot(data=subset(means, treatment!="drop"&treatment!="Unburned-N-L"&treatment!="Unburned-Nitrogen"&treatment!="Unburned-Litter"), aes(x=Year, y=Mtot, color=treatment))+
+  geom_point(size=2)+
+  geom_line(size=1)+
+  scale_color_manual(name="Treatment", breaks = c("Annual Burn","Annual+Nitrogen", "Annual+Litter", "Annual+N+L", "Unburned"), values=c("black", "red", "blue","darkorchid4", "gray47"))+
+  geom_errorbar(aes(ymin=Mtot-se, ymax=Mtot+se), width=0.1)+
+  ylab("Number of Stems")+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+  
+#unburned
+ggplot(data=subset(means, treatment!="drop"&treatment!="Annual+N+L"&treatment!="Annual+Nitrogen"&treatment!="Annual+Litter"), aes(x=Year, y=Mtot, color=treatment))+
+  geom_point(size=2)+
+  geom_line(size=1)+
+  scale_color_manual(name="Treatment", breaks = c("Annual Burn","Unburned-Nitrogen", "Unburned-Litter", "Unburned-N-L", "Unburned"), values=c("black", "red", "blue","darkorchid4", "gray47"))+
+  geom_errorbar(aes(ymin=Mtot-se, ymax=Mtot+se), width=0.1)+
+  ylab("Number of Stems")+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+         
