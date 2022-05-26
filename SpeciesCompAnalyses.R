2189afd2060368569863dbbfcc889749891bc778
Meghan Avolio <mavolio2@jh.edu>
2022-05-26 12:50:50 -0400

    analyzing data for the whole experiment and making figures

--- SpeciesCompAnalyses.R
+++ SpeciesCompAnalyses.R
@@ -7,9 +7,10 @@ library(stringr)
 library(lme4)
 library(lmerTest)
 library(vegan)
+library(emmeans)
 theme_set(theme_bw(20))
 
-sp<-read.csv("SpComp_2014-2018.CSV") %>%
+sp<-read.csv("SpComp_2014-2019.CSV") %>%
   mutate(plot=str_sub(Plot, -1)) %>% 
   select(-Plot) %>% 
   mutate(Plot=paste(Block, plot, sep=""))
@@ -26,16 +27,23 @@ richeven<-community_structure(sp2, time.var="Year", replicate.var = "plot_id", a
 ###analysis richness
 ######################################
 
-fit <- lmer(richness ~  Burn.Trt*Litter*Nutrient*Year +(1|Watershed/Block), data = richeven)
-anova(fit)
+fit <- lmer(richness ~  Burn.Trt*Litter*Nutrient*as.factor(Year) +(1|Watershed/Block), data = richeven)
+anova(fit, ddf="Kenward-Roger")
+
+emmeans(fit, pairwise~Burn.Trt*Litter, adjust="holm")
+emmeans(fit, pairwise~Nutrient, adjust="holm")
 
 ###analysis annual burn only
-fit1 <- lmer(richness ~  Litter*Nutrient*Year +(1|Watershed/Block), data = subset(richeven, Burn.Trt2==1))
+fit1 <- lmer(richness ~  Litter*Nutrient*as.factor(Year) +(1|Watershed/Block), data = subset(richeven, Burn.Trt2==1))
 anova(fit1)
 
 ###analysis of unburned only
-fit20 <- lmer(richness ~  Litter*Nutrient*Year +(1|Watershed/Block), data = subset(richeven, Burn.Trt2==20))
+fit20 <- lmer(richness ~  Litter*Nutrient*as.factor(Year) +(1|Watershed/Block), data = subset(richeven, Burn.Trt2==20))
 anova(fit20)
+
+##################
+########Doing the analysis of how to make annual to unburned and vice-versa
+####################
 
 ##plotting richness
 means<-richeven %>% 
@@ -75,7 +83,7 @@ r14<-richeven %>%
 fit14 <- lmer(richness ~  Burn.Trt2 +(1|Watershed/Block), data = r14)
 anova(fit14)
 
-#ther eis a difference in stems by watershed
+#there is a difference in stems by watershed
 summary(aov(richness~Watershed, data=r14))
 
 means14<-r14 %>% 
@@ -91,20 +99,66 @@ ggplot(data=means14, aes(x=Burn.Trt, y=mrich, fill=Burn.Trt))+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = "none")+
   scale_x_discrete(name="Burn Frequency", labels=c("Annual", "Unburned"))
 
+##############################
+#####Analysis of overall treatments
+###########################
+
+richMLit<-richeven %>% 
+  rename(Burn=Burn.Trt) %>% 
+  group_by(Burn, Litter) %>%
+  summarize(mrich=mean(richness, na.rm=T), sddev=sd(richness), n=length(richness)) %>% 
+  mutate(se=sddev/sqrt(n))
+
+ggplot(data=richMLit, aes(x=Burn, y=mrich, fill=Litter))+
+  geom_bar(stat="identity", position=position_dodge())+
+  geom_errorbar(aes(ymin=mrich-se, ymax=mrich+se), width=0.1, position=position_dodge(0.9))+
+  scale_fill_manual(values=c('gold', 'darkgoldenrod4'), labels=c("Absent", "Present"))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+  annotate("text", x=0.75, y=13, label="C", size=5)+
+  annotate("text", x=1.25, y=12, label="D", size=5)+
+  annotate("text", x=1.75, y=16, label="A", size=5)+
+  annotate("text", x=2.25, y=14, label="B", size=5)+
+  xlab('Burn Treatment')+
+  ylab('Species Richness')  
+
+richMN<-richeven %>% 
+  group_by(Nutrient) %>%
+  summarize(mrich=mean(richness, na.rm=T), sddev=sd(richness), n=length(richness)) %>% 
+  mutate(se=sddev/sqrt(n))
+
+ggplot(data=richMN, aes(x=Nutrient, y=mrich, fill=Nutrient))+
+  geom_bar(stat="identity")+
+  geom_errorbar(aes(ymin=mrich-se, ymax=mrich+se), width=0.1, position=position_dodge(0.9))+
+  scale_fill_manual(values=c("orangered", "orange", 'orangered4'))+
+  scale_x_discrete(limits=c("S", 'C', 'U'), labels=c("-N", 'C','+N'))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = 'none')+
+  annotate("text", x=1, y=14, label="A", size=5)+
+  annotate("text", x=2, y=14, label="A", size=5)+
+  annotate("text", x=3, y=13, label="B", size=5)+
+  xlab('Nutrient Treatment')+
+  ylab('Species Richness') 
+
 ###########################
 ##Evenness
 ############################
 
-fit <- lmer(Evar ~  Burn.Trt*Litter*Nutrient*Year +(1|Watershed/Block), data = richeven)
-anova(fit)
+fit <- lmer(Evar ~  Burn.Trt*Litter*Nutrient*as.factor(Year) +(1|Watershed/Block), data = richeven)
+anova(fit, ddf="Kenward-Roger")
+emmeans(fit, pairwise~Litter, adjust="holm")
+emmeans(fit, pairwise~Nutrient, adjust="holm")
+
 
 ###analysis annual burn only
-fit1 <- lmer(Evar ~  Litter*Nutrient*Year +(1|Watershed/Block), data = subset(richeven, Burn.Trt2==1))
+fit1 <- lmer(Evar ~  Litter*Nutrient*as.factor(Year) +(1|Watershed/Block), data = subset(richeven, Burn.Trt2==1))
 anova(fit1)
 
 ###analysis of unburned only
-fit20 <- lmer(Evar ~  Litter*Nutrient*Year +(1|Watershed/Block), data = subset(richeven, Burn.Trt2==20))
+fit20 <- lmer(Evar ~  Litter*Nutrient*as.factor(Year) +(1|Watershed/Block), data = subset(richeven, Burn.Trt2==20))
 anova(fit20)
+
+##################
+########Doing the analysis of how to make annual to unburned and vice-versa
+####################
 
 ####plotting evenness
 means<-richeven %>% 
@@ -159,6 +213,45 @@ ggplot(data=means14, aes(x=Burn.Trt, y=meven, fill=Burn.Trt))+
   ylab("Species Evenness")+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = "none")+
   scale_x_discrete(name="Burn Frequency", labels=c("Annual", "Unburned"))
+
+
+
+##############################
+#####Analysis of overall treatments
+###########################
+
+evenMLit<-richeven %>% 
+  group_by(Litter) %>%
+  summarize(meven=mean(Evar, na.rm=T), sddev=sd(Evar), n=length(Evar)) %>% 
+  mutate(se=sddev/sqrt(n))
+
+ggplot(data=evenMLit, aes(x=Litter, y=meven, fill=Litter))+
+  geom_bar(stat="identity")+
+  geom_errorbar(aes(ymin=meven-se, ymax=meven+se), width=0.1, position=position_dodge(0.9))+
+  scale_fill_manual(values=c("gold", "darkgoldenrod4"))+
+  scale_x_discrete(labels=c('Absent', 'Present'))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = 'none')+
+  annotate("text", x=1, y=0.38, label="A", size=5)+
+  annotate("text", x=2, y=0.38, label="B", size=5)+
+  xlab('Litter Treatment')+
+  ylab('Species Evenness') 
+
+evenMN<-richeven %>% 
+  group_by(Nutrient) %>%
+  summarize(meven=mean(Evar, na.rm=T), sddev=sd(Evar), n=length(Evar)) %>% 
+  mutate(se=sddev/sqrt(n))
+
+ggplot(data=evenMN, aes(x=Nutrient, y=meven, fill=Nutrient))+
+  geom_bar(stat="identity")+
+  geom_errorbar(aes(ymin=meven-se, ymax=meven+se), width=0.1, position=position_dodge(0.9))+
+  scale_fill_manual(values=c("orangered", "orange", 'orangered4'))+
+  scale_x_discrete(limits=c("S", 'C', 'U'), labels=c("-N", 'C','+N'))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = 'none')+
+  annotate("text", x=1, y=.4, label="A", size=5)+
+  annotate("text", x=2, y=.4, label="A", size=5)+
+  annotate("text", x=3, y=.35, label="B", size=5)+
+  xlab('Nutrient Treatment')+
+  ylab('Species Evenness') 
 
 
 ##############################
@@ -219,6 +312,62 @@ ggplot(data=scores, aes(x=MDS1, y=MDS2, color=Burn.Trt))+
   geom_point(size=3)+
   scale_color_manual(name="Burn Frequency", values=c('black', 'gray47'))+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-  
 
+####################
+####NMDS of all the data
+########################
+sp4<-sp2 %>% 
+  mutate(sp=paste(genus, species, sep="_")) %>% 
+  select(Burn.Trt, Nutrient, Litter, Year, Plot, sp, pcover) %>% 
+  pivot_wider(names_from='sp', values_from='pcover', values_fill = 0)
+
+##can't get model to converge with all data. Will try with in each burn trt.
+mds<-metaMDS(sp4[6:92],distance= "bray", trymax = 100)
+
+#running a model in unburned grasslands - no convergence for all years
+ub<-sp2 %>% 
+  mutate(sp=paste(genus, species, sep="_")) %>% 
+  filter(Burn.Trt=="Unburned") %>% 
+  select(Burn.Trt, Nutrient, Litter, Year, Plot, sp, pcover) %>% 
+  pivot_wider(names_from='sp', values_from='pcover', values_fill = 0)
+
+plotinfo=ub%>%
+  select(Burn.Trt, Nutrient, Litter, Year, Plot)
+
+mds<-metaMDS(ub[6:71],distance= "bray", trymax = 100)
+
+#####running a model for annually burned grasslands, also can't get convergence
+an<-sp2 %>% 
+  mutate(sp=paste(genus, species, sep="_")) %>% 
+  filter(Burn.Trt=="Annual") %>% 
+  select(Burn.Trt, Nutrient, Litter, Year, Plot, sp, pcover) %>% 
+  pivot_wider(names_from='sp', values_from='pcover', values_fill = 0)
+
+plotinfo=an%>%
+  select(Burn.Trt, Nutrient, Litter, Year, Plot)
+
+mds<-metaMDS(an[6:63],distance= "bray", trymax = 100)
+
+####trying with just pre-trt and 2019
+sp5<-sp2 %>% 
+  filter(Year==2014|Year==2019) %>% 
+  mutate(sp=paste(genus, species, sep="_")) %>% 
+  select(Burn.Trt, Nutrient, Litter, Year, Plot, sp, pcover) %>% 
+  pivot_wider(names_from='sp', values_from='pcover', values_fill = 0)
+
+##can't get model to converge with all data. Will try with in each burn trt.
+mds<-metaMDS(sp5[6:76],distance= "bray", trymax = 100)
+
+plotinfo=sp5%>%
+  select(Burn.Trt, Nutrient, Litter, Year, Plot)
+
+scores<-as.data.frame(mds$points) %>% 
+  bind_cols(plotinfo) %>% 
+  mutate(trt=paste(Nutrient, Litter, sep="_"))
+
+ggplot(data=scores, aes(x=MDS1, y=MDS2, color=as.factor(Year), shape=trt))+
+  geom_point(size=3)+
+  scale_color_manual(name="Burn Frequency", values=c('black', 'gray47'))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+  facet_wrap(~Burn.Trt, ncol=1)
 
