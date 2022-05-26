ec922ce38b294b6fce7d91c4eac9128d1d90612e
Meghan Avolio <mavolio2@jh.edu>
2022-05-26 12:50:16 -0400

    analyzing data for the whole experiment and making figures

--- BiomassAnalyses.R
+++ BiomassAnalyses.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(stringr)
 library(lme4)
 library(lmerTest)
+library(emmeans)
 theme_set(theme_bw(20))
 
 anpp<-read.csv("ANPP_2014-2019.CSV") %>% 
@@ -25,18 +26,23 @@ anpp2<-anpp %>%
 
 ###analysis of total biomass
 
-fit <- lmer(total ~  BurnFreq*Litter*Nutrient*Year +(1|Watershed/Block), data = anpp2)
-anova(fit)
+fit <- lmer(total ~  as.factor(BurnFreq)*Litter*Nutrient*as.factor(Year) +(1|Watershed/Block), data = anpp2)
+anova(fit,  ddf="Kenward-Roger")
+
+#doing contrasts 
+emmeans(fit, pairwise~Nutrient, adjust="holm")
 
 ###analysis annual burn only
-fit1 <- lmer(total ~  Litter*Nutrient*Year +(1|Watershed/Block), data = subset(anpp2, BurnFreq==1))
+fit1 <- lmer(total ~  Litter*Nutrient*as.factor(Year) +(1|Watershed/Block), data = subset(anpp2, BurnFreq==1))
 anova(fit1)
 
 ###analysis of unburned only
-fit20 <- lmer(total ~  Litter*Nutrient*Year +(1|Watershed/Block), data = subset(anpp2, BurnFreq==20))
+fit20 <- lmer(total ~  Litter*Nutrient*as.factor(Year) +(1|Watershed/Block), data = subset(anpp2, BurnFreq==20))
 anova(fit20)
 
-
+##################
+########Doing the analysis of how to make annual to unburned and vice-versa
+####################
 means<-anpp2 %>% 
   group_by(Year, BurnFreq, Litter, Nutrient) %>%
   summarize(Mtot=mean(total, na.rm=T), sddev=sd(total), n=length(total)) %>% 
@@ -64,9 +70,9 @@ ggplot(data=subset(means, treatment!="drop"&treatment!="Annual+N+L"&treatment!="
   ylab("ANPP")+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
-
+#########################
 ###2014 data
-
+########################
 b14<-anpp2 %>% 
   filter(Year==2014)
 
@@ -90,4 +96,34 @@ ggplot(data=means14, aes(x=as.factor(BurnFreq), y=mtot, fill=as.factor(BurnFreq)
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = "none")+
   scale_x_discrete(name="Burn Frequency", labels=c("Annual", "Unburned"))
 
+##############################
+#####Analysis of overall treatments
+###########################
 
+###there are interactive effects of year and burn trt, litter and nutrients
+meanN<-anpp2 %>% 
+  group_by(Nutrient) %>%
+  summarize(Mtot=mean(total, na.rm=T), sddev=sd(total), n=length(total)) %>% 
+  mutate(se=sddev/sqrt(n))
+
+ggplot(data=meanN, aes(x=Nutrient, y=Mtot, fill=Nutrient))+
+  geom_bar(stat="identity")+
+  geom_errorbar(aes(ymin=Mtot-se, ymax=Mtot+se), width=0.1, position=position_dodge(0.9))+
+  scale_x_discrete(limits=c("S", 'C', 'U'), labels=c("-N", 'C','+N'))+
+  scale_fill_manual(values=c("orangered", "orange", 'orangered4'))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position="none")+
+  annotate("text", x=1, y=450, label="C", size=5)+
+  annotate("text", x=2, y=550, label="B", size=5)+
+  annotate("text", x=3, y=600, label="A", size=5)+
+  xlab('Nutrient Treatment')+
+  ylab('ANPP')
+
+#not interested in this
+meanL<-anpp2 %>% 
+  group_by(Year, Litter) %>%
+  summarize(Mtot=mean(total, na.rm=T), sddev=sd(total), n=length(total)) %>% 
+  mutate(se=sddev/sqrt(n))
+
+ggplot(data=meanL, aes(x=Year, y=Mtot, color=Litter))+
+  geom_point()+
+  geom_line()
