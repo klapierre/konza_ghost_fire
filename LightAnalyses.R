4d230b55a76fce54e7cf41c7061c0a9d8229d998
Meghan Avolio <mavolio2@jh.edu>
2022-05-30 23:50:00 -0400

    removing 2014 in analyses, looking at effects of N addition on light

--- LightAnalyses.R
+++ LightAnalyses.R
@@ -1,6 +1,10 @@
 setwd("C:\\Users\\mavolio2\\Dropbox\\Konza Research\\GhostFire\\DATA\\Compiled data\\")
 
 library(tidyverse)
+library(lme4)
+library(lmerTest)
+library(vegan)
+library(emmeans)
 theme_set(theme_bw(20))
 
 light<-read.csv("Light_2014_2019.CSV")%>%
@@ -45,8 +49,27 @@ ggplot(data=means, aes(x=Year, y=Mlight, color=as.factor(Litter)))+
   facet_wrap(~WSBurn, ncol=1)
 
 ###analysis of light
-fit <- lmer(LightReduction ~  as.factor(Burn)*Litter*Nutrient*as.factor(Year) +(1|Watershed/Block), data = light3)
+fit <- lmer(LightReduction ~  as.factor(BurnFreq)*Litter*Nutrient*as.factor(Year) +(1|Watershed/Block), data = filter(light3, Year!=2014))
 anova(fit, ddf='Kenward-Roger')
 
 emmeans(fit, pairwise~Litter|as.factor(Burn), adjust="holm")
 emmeans(fit, pairwise~Nutrient*as.factor(Burn), adjust="holm")
+emmeans(fit, pairwise~Nutrient, adjust="holm")
+
+means<-light3 %>% 
+  group_by(Nutrient) %>%
+  summarize(Mlight=mean(LightReduction, na.rm=T), sdlight=sd(LightReduction, na.rm = T), n=length(LightReduction)) %>% 
+  mutate(se=sdlight/sqrt(n))
+
+ggplot(data=means, aes(x=Nutrient, y=Mlight, fill=Nutrient))+
+  geom_bar(stat="identity")+
+  geom_line()+
+  geom_errorbar(aes(ymin=Mlight-se, ymax=Mlight+se), width=0.1, postition=position_dodge(0.9))+
+  scale_fill_manual(values=c("orangered", "orange", 'orangered4'))+
+  scale_x_discrete(limits=c("S", 'C', 'U'), labels=c("-N", 'C','+N'))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = 'none')+
+  annotate("text", x=1, y=38, label="A", size=5)+
+  annotate("text", x=2, y=34, label="B", size=5)+
+  annotate("text", x=3, y=31, label="C", size=5)+
+  xlab('Nutrient Treatment')+
+  ylab("Pct. Light Reduction")
