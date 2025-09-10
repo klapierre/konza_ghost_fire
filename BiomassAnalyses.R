5df9307cc0140674746884a62efd160051774d32
Kevin Wilcox <wilcoxkr@gmail.com>
2025-09-10 17:38:00 -0400

    Brought in anpp data up to 2024, cleaned data, and combined. Individual years of data still need some additional cleaning

--- BiomassAnalyses.R
+++ BiomassAnalyses.R
@@ -1,4 +1,13 @@
+### Analyze and visualize ANPP data
+###
+### Last modified: Sept 10, 2025
+
+### 
+### Set up workspace
+###
+{
 setwd("C:\\Users\\mavolio2\\Dropbox\\Konza Research\\GhostFire\\DATA\\Compiled data\\")
+setwd("C:\\Users\\k_wilcox\\Dropbox\\Shared projects\\Ghost Fire\\DATA\\")
 
 library(tidyverse)
 library(stringr)
@@ -7,16 +16,132 @@ library(lmerTest)
 library(emmeans)
 theme_set(theme_bw(20))
 
-anpp<-read.csv("ANPP_2014-2019.CSV") %>% 
-  rename(plot=Plot) %>% 
-  mutate(Plot=paste(Block, plot, sep="")) %>% 
-  select(-plot)
-#check ANPP data
-ggplot(anpp, aes(x=total))+
+}
+
+
+###
+### Read in, clean, and combine all years of ANPP data
+###
+
+### NOTE: I STILL NEED TO GO THROUGH AND CLEAN EACH YEAR OF DATA BEFORE COMBINING TO GET PLOT MEANS
+anpp_14_21 <- read.csv("Compiled data\\ANPP_2014-2021_GFPW.CSV") %>% 
+#  rename(plot=Plot) %>% 
+  mutate(
+    QC_flag=NA,
+    PlotID=paste(Watershed, Block, Plot, sep="_"),
+    BlockID=paste(Watershed, Block, sep="_")
+    ) %>%
+  dplyr::select(Year, BurnFreq, Watershed, Block, Plot, PlotID, BlockID,
+                Grass, Forb, Woody, P.Dead, QC_flag)
+
+## 2022
+anpp_22 <- read.csv("GhostFire2022_Data\\Biomass\\GhostFire_Biomass_Data_2022.csv") %>%
+  rename(Watershed=Wateshed) %>%
+  separate_wider_position(Plot, c(Block=1, Plot=1)) %>%
+  mutate(
+    PlotID=paste(Watershed, Block, Plot, sep="_"),
+    BlockID=paste(Watershed, Block, sep="_"),
+    Plot=as.numeric(Plot)
+  ) %>%
+  filter(QC_flag!=1) %>%
+  group_by(Year, BurnFreq, Watershed, Block, Plot, PlotID, BlockID) %>%
+  summarize(
+    Grass = sum(Grass, na.rm=T), # assumes NAs represent 0s, but we should change this if that's not the case
+    Forb = sum(Forb, na.rm=T),# assumes NAs represent 0s, but we should change this if that's not the case
+    Woody = sum(Woody, na.rm=T),
+    P.Dead = sum(P.Dead, na.rm=T),
+    QC_flag = max(QC_flag, na.rm=T),
+    .groups="drop"
+#    Notes = paste(Notes, sep="_") # doesn't work, I think we would need to use reframe to do this
+    )
+
+## 2023
+anpp_23 <- read.csv("GhostFire2023_Data\\Biomass\\GhostFire_Biomass_Data_2023.csv") %>%
+  separate_wider_position(Plot, c(Block=1, Plot=1)) %>%
+  mutate(
+    PlotID=paste(Watershed, Block, Plot, sep="_"),
+    BlockID=paste(Watershed, Block, sep="_"),
+    Plot=as.numeric(Plot)
+  ) %>%
+  filter(QC_flag!=1) %>%
+  group_by(Year, BurnFreq, Watershed, Block, Plot, PlotID, BlockID) %>%
+  summarize(
+    Grass = sum(Grass, na.rm=T), # assumes NAs represent 0s, but we should change this if that's not the case
+    Forb = sum(Forb, na.rm=T),# assumes NAs represent 0s, but we should change this if that's not the case
+    Woody = sum(Woody, na.rm=T),
+    P.Dead = sum(P.Dead, na.rm=T),
+    QC_flag = max(QC_flag, na.rm=T),
+    .groups="drop"
+    #    Notes = paste(Notes, sep="_") # doesn't work, I think we would need to use reframe to do this
+  )
+
+## 2024
+anpp_24 <- read.csv("GhostFire2024_Data\\Biomass\\GhostFire_ANPP_2024.csv") %>%
+  rename(P.Dead=Pdead) %>%
+  mutate(
+    PlotID=paste(Watershed, Block, Plot, sep="_"),
+    BlockID=paste(Watershed, Block, sep="_"),
+    Plot=as.numeric(Plot)
+  ) %>%
+  mutate(QC_flag=0) %>%
+  group_by(Year, BurnFreq, Watershed, Block, Plot, PlotID, BlockID) %>%
+  summarize(
+    Grass = sum(Grass, na.rm=T), # assumes NAs represent 0s, but we should change this if that's not the case
+    Forb = sum(Forb, na.rm=T),# assumes NAs represent 0s, but we should change this if that's not the case
+    Woody = sum(Woody, na.rm=T),
+    P.Dead = sum(P.Dead, na.rm=T),
+    QC_flag = max(QC_flag, na.rm=T),
+    .groups="drop"
+    #    Notes = paste(Notes, sep="_") # doesn't work, I think we would need to use reframe to do this
+  )
+
+
+
+
+### Combine all years together
+anpp_14_24 <- anpp_14_21 %>% 
+  bind_rows(
+    anpp_22,
+    anpp_23,
+    anpp_24)
+
+## Make sure all years have the same plot identifiers
+unique(anpp_14_24$Year)
+unique(anpp_14_24$BurnFreq)
+unique(anpp_14_24$Watershed)
+unique(anpp_14_24$Block)
+unique(anpp_14_24$Plot)
+
+with(anpp_14_24, table(Year, Block)) # 2017 only has one value and I think we didn't sample ANPP in that year
+## there are a few mixxing plots in 2022, and in 2018... could look into these - I think the 2018 might be the missing bag of ANPP that happened :(
+
+unique(anpp_14_21$Year)
+unique(anpp_22$Year)
+unique(anpp_23$Year)
+unique(anpp_24$Year) ## It's here in 2024 -- I check in the raw data and its clearly a data entry error, fixed it in the excel spreadsheet and csv files
+
+filter(anpp_14_24, BlockID=="20C_C")
+filter(anpp_14_24, Grass>500)
+filter(anpp_22, PlotID=="1D_B_5")
+  #check ANPP data
+ggplot(anpp_14_24, aes(x=Grass))+
   geom_histogram()+
   facet_wrap(~Year)
 
-trts<-read.csv("GF_PlotList_Trts.csv") %>% 
+ggplot(anpp_14_24, aes(x=Forb))+
+  geom_histogram()+
+  facet_wrap(~Year)
+
+ggplot(anpp_14_24, aes(x=Woody))+
+  geom_histogram()+
+  facet_wrap(~Year)
+
+ggplot(anpp_14_24, aes(x=P.Dead))+
+  geom_histogram()+
+  facet_wrap(~Year)
+
+### Left off here
+trt_key<-read.csv("GF_PlotList_Trts.csv") %>% 
   select(-plot_id) %>% 
   rename(BurnFreq=Burn.Trt2)%>%
   select(-Burn.Trt) 
