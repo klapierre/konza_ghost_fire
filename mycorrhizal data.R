8ae584fe974113b5afbcf6b0e9e9d19947a83aa8
Meghan Avolio <meghan.avolio@gmail.com>
2025-11-05 15:20:13 -0500

    importing mycorrhizal data

---
+++ mycorrhizal data.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(codyn)
+library(stringr)
+library(vegan)
+library(emmeans)
+theme_set(theme_bw(20))
+
+setwd("C:\\Users\\mavolio2\\Dropbox\\Konza Research\\GhostFire\\DATA\\")
+
+trts<-read.csv("Compiled data\\GF_PlotList_Trts.csv") %>% 
+  rename(PlotID=Plot,
+         Plot=plot_id)
+
+myc2014<-read.csv('GhostFire2014_Data\\Mycorr\\MycoCounts_GF_2014.csv') %>% 
+  mutate(Year=2014) %>% 
+  select(Year, Watershed, Block, Plot, Average) %>% 
+  rename(Colonization=Average)
+myc2019<-read.csv('GhostFire2019_Data\\Mycorr\\GF_MycorrhizalCounts_2019.csv') %>% 
+  separate(Sample, into=c('Block', 'Plot'), sep=1, remove = F) %>% 
+  select(Year, Watershed, Block, Plot, Colonization) %>% 
+  mutate(Plot=as.numeric(Plot))
+myc2024<-read.csv('GhostFire2024_Data\\Mycorr\\GF_Mycorrhizal_2024.csv') %>% 
+  separate(Sample, into=c('Block', 'Plot'), sep=1, remove = F) %>% 
+  select(Year, Watershed, Block, Plot, Colonization) %>% 
+  mutate(Plot=as.numeric(Plot))
+
+alldat<-myc2014 %>% 
+  bind_rows(myc2019, myc2024) %>% 
+  group_by(Year, Watershed, Block, Plot) %>% 
+  summarise(Colonization=mean(Colonization)) %>% 
+  pivot_wider(names_from = 'Year', names_prefix = 'Y', values_from = 'Colonization', values_fill =NA)
